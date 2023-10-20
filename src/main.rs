93c22f85ab3064d8e6c925e5c504e695d10276d5
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-10-19 22:37:29 -0400

    Covert conditional statements to match cases

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 use serde_json;
 use std::env;
 
-// Available if you need it!
-// use serde_bencode
-
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else if encoded_value.starts_with('i') {
-        let end_index = encoded_value.find('e').unwrap();
-        let encoded_number = &encoded_value[1..end_index];
-        let number: i64 = encoded_number.parse().unwrap();
-        let serde_number = serde_json::Number::from(number);
-        return serde_json::Value::Number(serde_number);
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    match encoded_value.chars().next() {
+        Some('0'..='9') => {
+            // Example: "5:hello" -> "hello"
+            let colon_index = encoded_value.find(':').unwrap();
+            let number_string = &encoded_value[..colon_index];
+            let number = number_string.parse::<i64>().unwrap();
+            let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+            return serde_json::Value::String(string.to_string());
+        }
+        Some('i') => {
+            // Example: "i-35e" -> "-35"
+            let end_index = encoded_value.find('e').unwrap();
+            let encoded_number = &encoded_value[1..end_index];
+            let number: i64 = encoded_number.parse().unwrap();
+            let serde_number = serde_json::Number::from(number);
+            return serde_json::Value::Number(serde_number);
+        }
+        _ => {
+            panic!("Unhandled encoded value: {}", encoded_value)
+        }
     }
 }
 
