8bb607f32720ef319a014143a80a7256c4fff69c
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-10-20 01:56:28 -0400

    Consume token when parsing

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
 use serde_json;
+use serde_json::json;
 use std::env;
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
     match encoded_value.chars().next() {
         Some('0'..='9') => {
             // Example: "5:hello" -> "hello"
-            let colon_index = encoded_value.find(':').unwrap();
-            let number_string = &encoded_value[..colon_index];
-            let number = number_string.parse::<i64>().unwrap();
-            let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-            return serde_json::Value::String(string.to_string());
+            if let Some((size, rest)) = encoded_value.split_once(":") {
+                if let Ok(size) = size.parse::<usize>() {
+                    let (word, rest) = rest.split_at(size);
+                    (json!(word), rest)
+                } else {
+                    panic!("Error extracting word of size {}", size)
+                }
+            } else {
+                panic!("Error parsing string")
+            }
         }
         Some('i') => {
             // Example: "i-35e" -> "-35"
-            let end_index = encoded_value.find('e').unwrap();
-            let encoded_number = &encoded_value[1..end_index];
-            let number: i64 = encoded_number.parse().unwrap();
-            let serde_number = serde_json::Number::from(number);
-            return serde_json::Value::Number(serde_number);
+            if let Some((number, rest)) = encoded_value.split_at(1).1.split_once("e") {
+                let number = number.parse::<i64>().unwrap();
+                (json!(number), rest)
+            } else {
+                panic!("Error retrieving number")
+            }
         }
         _ => {
             panic!("Unhandled encoded value: {}", encoded_value)
@@ -33,7 +40,7 @@ fn main() {
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let (decoded_value, _) = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
@@ -43,15 +50,24 @@ fn main() {
 #[cfg(test)]
 mod test {
     use super::*;
+    use serde_json::json;
+
+    fn decode_bencode_test_wrapper(input: &str, expected: (serde_json::Value, &str)) {
+        let actual = decode_bencoded_value(input);
+        assert_eq!(expected, actual);
+    }
 
     #[test]
     fn decode_bencode_string() {
-        assert_eq!(decode_bencoded_value("5:hello"), "hello")
+        decode_bencode_test_wrapper("5:hello", (json!("hello"), ""));
+        decode_bencode_test_wrapper("12:hello world!", (json!("hello world!"), ""));
+        decode_bencode_test_wrapper("7:consumeremain", (json!("consume"), "remain"))
     }
 
     #[test]
     fn decode_bencode_integer() {
-        assert_eq!(decode_bencoded_value("i32e"), 32);
-        assert_eq!(decode_bencoded_value("i-32e"), -32);
+        decode_bencode_test_wrapper("i36e", (json!(36), ""));
+        decode_bencode_test_wrapper("i-36e", (json!(-36), ""));
+        decode_bencode_test_wrapper("i10eremain", (json!(10), "remain"));
     }
 }
