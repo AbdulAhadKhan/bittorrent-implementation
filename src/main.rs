8e57c608b8708c456319926e305f1262886b4071
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-10-20 03:42:18 -0400

    Return result from decode_bencoded_value for better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,33 @@
+use anyhow::anyhow;
 use serde_json;
 use serde_json::json;
 use std::env;
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
+fn decode_bencoded_value(encoded_value: &str) -> Result<(serde_json::Value, &str), anyhow::Error> {
     match encoded_value.chars().next() {
         Some('0'..='9') => {
             // Example: "5:hello" -> "hello"
-            if let Some((size, rest)) = encoded_value.split_once(":") {
-                if let Ok(size) = size.parse::<usize>() {
+            match encoded_value.split_once(":") {
+                Some((size, rest)) => {
+                    let size = size.parse::<usize>()?;
                     let (word, rest) = rest.split_at(size);
-                    (json!(word), rest)
-                } else {
-                    panic!("Error extracting word of size {}", size)
+                    anyhow::Ok((json!(word), rest))
                 }
-            } else {
-                panic!("Error parsing string")
+                None => Err(anyhow!("Invalid bencode syntax: {}", encoded_value)),
             }
         }
         Some('i') => {
             // Example: "i-35e" -> "-35"
-            if let Some((number, rest)) = encoded_value.split_at(1).1.split_once("e") {
-                let number = number.parse::<i64>().unwrap();
-                (json!(number), rest)
-            } else {
-                panic!("Error retrieving number")
+            match encoded_value.split_at(1).1.split_once("e") {
+                Some((number, rest)) => {
+                    let number = number.parse::<i64>().unwrap();
+                    anyhow::Ok((json!(number), rest))
+                }
+                None => Err(anyhow!("Invalid bencode syntax: {}", encoded_value)),
             }
         }
-        _ => {
-            panic!("Unhandled encoded value: {}", encoded_value)
-        }
+        _ => Err(anyhow!("Unhandled encoded value: {}", encoded_value)),
     }
 }
 
@@ -40,8 +38,8 @@ fn main() {
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let (decoded_value, _) = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        let decoded_value = decode_bencoded_value(encoded_value).unwrap();
+        println!("{}", decoded_value.1.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
@@ -54,7 +52,7 @@ mod test {
 
     fn decode_bencode_test_wrapper(input: &str, expected: (serde_json::Value, &str)) {
         let actual = decode_bencoded_value(input);
-        assert_eq!(expected, actual);
+        assert_eq!(expected, actual.unwrap());
     }
 
     #[test]
