aefcad182cce8c1082a2f8306d10c2eea8b64269
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-10-20 23:38:10 -0400

    Add tests and debug

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,13 @@ fn decode_bencoded_value(encoded_value: &str) -> Result<(serde_json::Value, &str
             match encoded_value.split_once(":") {
                 Some((size, rest)) => {
                     let size = size.parse::<usize>()?;
+                    if size > rest.len() {
+                        return Err(anyhow!(
+                            "Specified string '{}' is greater than size of {}",
+                            rest,
+                            size
+                        ));
+                    }
                     let (word, rest) = rest.split_at(size);
                     anyhow::Ok((json!(word), rest))
                 }
@@ -21,7 +28,7 @@ fn decode_bencoded_value(encoded_value: &str) -> Result<(serde_json::Value, &str
             // Example: "i-35e" -> "-35"
             match encoded_value.split_at(1).1.split_once("e") {
                 Some((number, rest)) => {
-                    let number = number.parse::<i64>().unwrap();
+                    let number = number.parse::<i64>()?;
                     anyhow::Ok((json!(number), rest))
                 }
                 None => Err(anyhow!("Invalid bencode syntax: {}", encoded_value)),
@@ -46,7 +53,7 @@ fn main() {
 }
 
 #[cfg(test)]
-mod test {
+mod bencode_tests {
     use super::*;
     use serde_json::json;
 
@@ -59,7 +66,13 @@ mod test {
     fn decode_bencode_string() {
         decode_bencode_test_wrapper("5:hello", (json!("hello"), ""));
         decode_bencode_test_wrapper("12:hello world!", (json!("hello world!"), ""));
-        decode_bencode_test_wrapper("7:consumeremain", (json!("consume"), "remain"))
+        decode_bencode_test_wrapper("7:consumeremain", (json!("consume"), "remain"));
+    }
+
+    #[test]
+    fn decode_bencode_string_error() {
+        assert!(decode_bencoded_value("5:").is_err());
+        assert!(decode_bencoded_value(":fail").is_err());
     }
 
     #[test]
@@ -67,5 +80,10 @@ mod test {
         decode_bencode_test_wrapper("i36e", (json!(36), ""));
         decode_bencode_test_wrapper("i-36e", (json!(-36), ""));
         decode_bencode_test_wrapper("i10eremain", (json!(10), "remain"));
+    }
+
+    #[test]
+    fn decode_bencode_integer_error() {
+        assert!(decode_bencoded_value("ie").is_err());
     }
 }
