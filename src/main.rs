8731d2385fe7a4399974afeff57d8d85e62c2111
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-10-23 20:30:53 -0400

    Covert arg parsing to clap

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 mod bencode;
 
 use bencode::decode_bencoded_value;
-use std::env;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about=None)]
+enum Args {
+    Decode { bencode: String },
+    Info { torrent_file: String },
+}
+
+fn request_bencode_decode(encoded_value: &str) {
+    let decoded_value = decode_bencoded_value(encoded_value).unwrap();
+    println!("{}", decoded_value.0.to_string());
+}
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
+    let arg = Args::parse();
 
-    if command == "decode" {
-        let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value).unwrap();
-        println!("{}", decoded_value.0.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+    match &arg {
+        Args::Decode { bencode } => {
+            request_bencode_decode(bencode);
+        }
+        Args::Info { torrent_file } => {
+            println!("{:?}", torrent_file);
+        }
     }
 }
