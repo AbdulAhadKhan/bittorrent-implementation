d84136dc778c213f3da5cd03ee6100c48e13dcfe
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-10-24 05:10:14 -0400

    Get individual pieces' hashes

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,17 @@ fn request_torrent_info(path: &str) {
     let torrent_info = Torrent::new(path).unwrap();
     let info_hash = torrent_info.get_info_hash();
     let info_hash = hex::encode(info_hash);
+    let pieces_hases = torrent_info.get_pieces_hashes();
 
     println!("Tracker URL: {}", torrent_info.announce);
     println!("Length: {}", torrent_info.info.length);
     println!("Info Hash: {}", info_hash);
+    println!("Piece Length: {}", torrent_info.info.piece_length);
+    println!("Pieces Hashes:");
+    for i in 0..pieces_hases.len() {
+        let hash_hex = hex::encode(&pieces_hases[i]);
+        println!("{}", hash_hex);
+    }
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
