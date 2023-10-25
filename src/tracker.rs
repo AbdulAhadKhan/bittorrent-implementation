873c700cbdb2af8bd693255f50372d28118a4302
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-10-25 15:11:47 -0400

    GET tracker information

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,19 +1,35 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_bytes::ByteBuf;
 
 #[derive(Debug, Serialize)]
 pub struct TrackerRequest {
-    info_hash: ByteBuf,
-    peer_id: String,
-    port: u16,
-    uploaded: u64,
-    downloaded: u64,
-    left: u64,
-    compact: u8,
+    pub peer_id: String,
+    pub port: u16,
+    pub uploaded: u64,
+    pub downloaded: u64,
+    pub left: usize,
+    pub compact: u8,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TrackerResponse {
+    interval: usize,
+    peers: ByteBuf,
 }
 
 impl TrackerRequest {
-    fn get(&self, url: &str) {
-        println!("{}", url);
+    pub async fn get(&self, url: &str, info_hash: &str) -> TrackerResponse {
+        let params = serde_urlencoded::to_string(&self).unwrap();
+        let tracker_url = format!("{}?info_hash={}&{}", url, info_hash, params);
+
+        let response = reqwest::get(&tracker_url)
+            .await
+            .unwrap()
+            .bytes()
+            .await
+            .unwrap();
+
+        let response: TrackerResponse = serde_bencode::from_bytes(&response).unwrap();
+        response
     }
 }
