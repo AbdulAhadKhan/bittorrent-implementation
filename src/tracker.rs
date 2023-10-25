07aead0137002a7450a5f04f107761ca51f179d0
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-10-25 19:47:05 -0400

    Translate recieved binary peer addresses into IPv4 address and port vector

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 use serde_bytes::ByteBuf;
+use std::convert::TryInto;
+use std::net::Ipv4Addr;
 
 #[derive(Debug, Serialize)]
 pub struct TrackerRequest {
@@ -13,9 +15,11 @@ pub struct TrackerRequest {
 
 #[derive(Debug, Deserialize)]
 pub struct TrackerResponse {
-    interval: usize,
-    peers: ByteBuf,
+    pub interval: usize,
+    pub peers: ByteBuf,
 }
+
+type AddressList = Vec<(Ipv4Addr, u16)>;
 
 impl TrackerRequest {
     pub async fn get(&self, url: &str, info_hash: &str) -> Result<TrackerResponse, anyhow::Error> {
@@ -26,5 +30,24 @@ impl TrackerRequest {
         let response: TrackerResponse = serde_bencode::from_bytes(&response)?;
 
         anyhow::Ok(response)
+    }
+}
+
+impl TrackerResponse {
+    pub fn get_peers_address(&self) -> Result<AddressList, anyhow::Error> {
+        let mut addresses: AddressList = Vec::new();
+
+        for i in 0..(&self.peers.len() / 6) {
+            let increment = i * 6;
+            let end = increment + 6;
+
+            let [a, b, c, d, port_1, port_2]: [_; 6] = self.peers[increment..end].try_into()?;
+            let ipv4 = Ipv4Addr::new(a, b, c, d);
+            let port = u16::from_be_bytes([port_1, port_2]);
+
+            addresses.push((ipv4, port));
+        }
+
+        anyhow::Ok(addresses)
     }
 }
