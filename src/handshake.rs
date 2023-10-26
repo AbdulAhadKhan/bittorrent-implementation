c2cdc7b9df18a389686e2f1bd949aba5cc34ebff
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-10-26 19:03:40 -0400

    Build new handshake object

--- src/handshake.rs
+++ src/handshake.rs
@@ -1,6 +1,7 @@
 use tokio::net::TcpStream;
 
 #[allow(unused)]
+#[derive(Debug)]
 pub struct Handshake<'a> {
     reserved: u64,
     info_hash: &'a [u8; 20],
@@ -12,7 +13,9 @@ impl Handshake<'_> {
     const LENGTH: u8 = 19;
     const PROTOCOL_STRING: &[u8; 19] = b"BitTorrent protocol";
 
-    pub fn new<'a>(info_hash: &'a [u8; 20], peer_id: &'a [u8; 20]) -> Handshake<'a> {
+    pub fn new<'a>(info_hash: &'a [u8; 20], peer_id: &'a str) -> Handshake<'a> {
+        let peer_id: &[u8; 20] = peer_id.as_bytes().try_into().unwrap();
+
         Handshake {
             reserved: 0,
             info_hash,
